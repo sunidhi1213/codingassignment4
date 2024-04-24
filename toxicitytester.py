95579b45204a81c12aae7d3ee6255c0d5639ae9b
sunidhi1213 <166869722+sunidhi1213@users.noreply.github.com>
2024-04-23 17:37:10 -0700

    Add files via upload
    python file uploaded

---
+++ toxicitytester.py
@@ -0,0 +1,21 @@
+from googleapiclient import discovery
+import json
+
+API_KEY = 'AIzaSyByyojff84-YS56sxa8oAnuFeac8b_t6po'
+
+client = discovery.build(
+  "commentanalyzer",
+  "v1alpha1",
+  developerKey=API_KEY,
+  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+  static_discovery=False,
+)
+
+analyze_request = {
+  'comment': { 'text': 'friendly greetings from python' },
+  'requestedAttributes': {'TOXICITY': {}}
+}
+
+response = client.comments().analyze(body=analyze_request).execute()
+print(response)
+
